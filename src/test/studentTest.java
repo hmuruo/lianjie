46d3ff2ed9c44a85bb2c788521b170c0622afd62
hmuruo <42560731+hmuruo@users.noreply.github.com>
2018-08-22 11:45:30 +0800

    Update studentTest.java
    StudentTest提交2

--- src/test/studentTest.java
+++ src/test/studentTest.java
@@ -3,6 +3,7 @@ package test;
 public class studentTest {
     public static void main(String[] args) {
         System.out.println("1.11111111111111111111");
+        System.out.println("2.22222222222222222222");
     }
 
 
